0255f4187b87beef5fe0550f6771796f8c62db1f
Jairo0811 <jairo_1829@hotmail.com>
2026-06-22 15:30:36 -0400

    Update main.cpp

--- legacy/main.cpp
+++ legacy/main.cpp
@@ -1,253 +1,278 @@
 #include <iostream>
 #include <string>
-#include <stdlib.h>
-#include <conio.h>
-#include <stdio.h>
+#include <limits>
+
 using namespace std;
 
-struct Nodo{
+struct Nodo {
     string nombre;
-    Nodo *siguiente;
+    Nodo* siguiente;
 };
-
-
 
 class Paises {
 private:
-    Nodo *cabeza, *cola;
+    Nodo* cabeza;
+    Nodo* cola;
 
 public:
-    Paises(){
-    cabeza = NULL;
-    cola = NULL;
+    Paises() {
+        cabeza = NULL;
+        cola = NULL;
     }
 
+    ~Paises() {
+        Nodo* actual = cabeza;
 
-////////////////////////////////////////////////////////////////////////////////
-
-    void introducirDestino(string pais){
-    Nodo *temp = new Nodo;
-    temp-> nombre = pais;
-    temp-> siguiente = NULL;
-        if(cabeza == NULL){
-            cabeza = temp;
-            cola = temp;
-            temp = NULL;
-        } else {
-        cola -> siguiente = temp;
-        cola = temp;
+        while (actual != NULL) {
+            Nodo* siguiente = actual->siguiente;
+            delete actual;
+            actual = siguiente;
         }
     }
 
-//////////////////////////////////////////////////////////////////
-    bool tiene_origen_o_destino(string origen ,string destino){
-        Nodo * temp = new Nodo;
-        temp = cabeza;
-        bool esVerdad = false;
-        int aiuda = 0;
-        while(temp != NULL){
-            if(temp-> nombre ==  origen){
-                aiuda++;
+    void introducirDestino(const string& pais) {
+        Nodo* nuevo = new Nodo;
+        nuevo->nombre = pais;
+        nuevo->siguiente = NULL;
+
+        if (cabeza == NULL) {
+            cabeza = nuevo;
+            cola = nuevo;
+        } else {
+            cola->siguiente = nuevo;
+            cola = nuevo;
+        }
+    }
+
+    bool existeDestino(const string& pais) const {
+        Nodo* temp = cabeza;
+
+        while (temp != NULL) {
+            if (temp->nombre == pais) {
+                return true;
             }
 
-            if(temp -> nombre == destino){
-                aiuda++;
-            }
-
-            if(aiuda == 2){
-                esVerdad = true;
-                break;
-            }
             temp = temp->siguiente;
         }
-        return esVerdad;
+
+        return false;
     }
 
-//////////////////////////////////////////////////////////////////
-
-    int cuenta (string origen, string destino){
-
-    int cont = 0;
-    if(origen == destino){
-    return 0;
+    bool tieneOrigenYDestino(const string& origen, const string& destino) const {
+        return existeDestino(origen) && existeDestino(destino);
     }
-    Nodo *temp = new Nodo;
-    temp = cabeza;
-    Nodo* principio = new Nodo;
-    principio = cabeza;
-        while(temp != NULL){
 
-                if(temp -> nombre == origen) {
-                    cont++;
-                    temp = temp->siguiente;
-                    continue;
-                }
-
-
-                if(temp-> nombre == destino && cont > 0){
-                    cont++;
-                    break;
-                }
-
-                if(cont > 0 ){
-                    cont++;
-                    temp = temp->siguiente;
-                    continue;
-                }
-
-                temp = temp -> siguiente;
-
-                if(temp -> siguiente == NULL){
-                    temp->siguiente = principio;
-                    temp = temp->siguiente;
-                }
-
+    int contarVuelos(const string& origen, const string& destino) const {
+        if (!tieneOrigenYDestino(origen, destino)) {
+            return -1;
         }
-    return cont -= 2;
-    }
 
-////////////////////////////////////////////////////////////////////////
-
-    string buscarRuta(string origen, string destino){
-
-    string ruta = "";
-    int coun = 0;
-
-    if(!tiene_origen_o_destino(origen,destino)){
-        ruta = "El origen o el destino que ingresaste no existe";
-        return ruta;
-    }
-
-    if(origen == destino){
-    ruta = "Estas en ese lugar";
-    return ruta;
-//    cout<<ruta<<endl;
-    }
-
-    Nodo *temp = new Nodo;
-    temp = cabeza;
-    Nodo* principio = new Nodo;
-    principio = cabeza;
-        while(temp != NULL){
-
-                if(temp -> nombre == origen) {
-                    coun++;
-                    ruta += temp->nombre;
-                    temp = temp->siguiente;
-                    ruta += " Hacia ";
-                    ruta += temp->nombre;
-                    continue;
-                }
-
-                if(temp-> nombre == destino){
-
-                    break;
-                }
-
-                if(coun > 0 ){
-                    coun++;
-                    ruta += "\n";
-                    ruta += temp->nombre;
-                    ruta += " Hacia ";
-                    temp = temp->siguiente;
-                    ruta += temp->nombre;
-                    continue;
-                }
-
-
-
-                temp = temp -> siguiente;
-                if(temp -> siguiente == NULL){
-                    temp->siguiente = principio;
-                    temp = temp->siguiente;
-                }
-
+        if (origen == destino) {
+            return 0;
         }
-      return ruta;
+
+        Nodo* temp = cabeza;
+        bool contando = false;
+        int vuelos = 0;
+
+        while (temp != NULL) {
+            if (temp->nombre == origen) {
+                contando = true;
+            }
+
+            if (contando && temp->siguiente != NULL) {
+                vuelos++;
+            }
+
+            if (contando && temp->siguiente != NULL && temp->siguiente->nombre == destino) {
+                return vuelos;
+            }
+
+            temp = temp->siguiente;
+        }
+
+        // Ruta circular: último destino conecta con el primero.
+        if (contando && cola != NULL && cabeza != NULL) {
+            vuelos++;
+
+            if (cabeza->nombre == destino) {
+                return vuelos;
+            }
+
+            temp = cabeza;
+
+            while (temp != NULL && temp->nombre != origen) {
+                if (temp->siguiente != NULL && temp->siguiente->nombre == destino) {
+                    return vuelos + 1;
+                }
+
+                vuelos++;
+                temp = temp->siguiente;
+            }
+        }
+
+        return -1;
     }
 
+    string buscarRuta(const string& origen, const string& destino) const {
+        if (!tieneOrigenYDestino(origen, destino)) {
+            return "El origen o el destino ingresado no existe.";
+        }
+
+        if (origen == destino) {
+            return "Ya estas en ese destino.";
+        }
+
+        string ruta = "";
+        Nodo* temp = cabeza;
+        bool construyendo = false;
+
+        while (temp != NULL) {
+            if (temp->nombre == origen) {
+                construyendo = true;
+            }
+
+            if (construyendo && temp->siguiente != NULL) {
+                ruta += temp->nombre + " hacia " + temp->siguiente->nombre + "\n";
+
+                if (temp->siguiente->nombre == destino) {
+                    return ruta;
+                }
+            }
+
+            temp = temp->siguiente;
+        }
+
+        // Ruta circular: último destino conecta con el primero.
+        if (construyendo && cola != NULL && cabeza != NULL) {
+            ruta += cola->nombre + " hacia " + cabeza->nombre + "\n";
+
+            if (cabeza->nombre == destino) {
+                return ruta;
+            }
+
+            temp = cabeza;
+
+            while (temp != NULL && temp->nombre != origen) {
+                if (temp->siguiente != NULL) {
+                    ruta += temp->nombre + " hacia " + temp->siguiente->nombre + "\n";
+
+                    if (temp->siguiente->nombre == destino) {
+                        return ruta;
+                    }
+                }
+
+                temp = temp->siguiente;
+            }
+        }
+
+        return "No se encontro una ruta disponible.";
+    }
+
+    void listarDestinos() const {
+        Nodo* temp = cabeza;
+
+        cout << "\nDestinos registrados:\n";
+
+        while (temp != NULL) {
+            cout << "- " << temp->nombre << endl;
+            temp = temp->siguiente;
+        }
+
+        cout << endl;
+    }
 };
 
-void ingresaEnter(){
-while(true){
-         cout << "Presiona Enter para ir al menu principal" << endl;
-         if(getch() == 13){
-            break;
-         }
-}
-}
-
-string preguntador(){
-string hihi;
-getline(cin,hihi);
-return hihi;
+void limpiarPantalla() {
+#ifdef _WIN32
+    system("cls");
+#else
+    system("clear");
+#endif
 }
 
-Paises paises;
+void pausar() {
+    cout << "\nPresiona Enter para continuar...";
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+}
 
+string leerTexto(const string& mensaje) {
+    string texto;
 
+    cout << mensaje;
+    getline(cin, texto);
 
-int main()
-{
+    return texto;
+}
 
+int main() {
+    Paises paises;
 
     paises.introducirDestino("miami");
     paises.introducirDestino("orlando");
     paises.introducirDestino("santo domingo");
     paises.introducirDestino("san francisco");
 
-    string origen, destino;
-    string s,j;
-    int finaI;
- //   cout << paises.tiene_origen_o_destino("miami","santo domingo");
- //   cout << paises.tiene_origen_o_destino("sad","santo domingo") << endl;
-        while(true){
-    string respuesta;
-    cout << "Aerolinea" << endl;
-    cout << "1-Introducir destino" << endl;
-    cout << "2-Introducir ruta" << endl;
-    cout << "3-Buscar Ruta" << endl ;
-    cout << "Introduzca la opcion que desee: ";
-    getline(cin,respuesta);
+    string opcion;
 
-    if(respuesta == "1"){
+    do {
+        limpiarPantalla();
 
-                system("cls");
-                cout<<"Ingrese el pais nuevo"<<endl;
-                cin>>destino;
-                cout<<"Agregado correctamente\n\n\n";
-                paises.introducirDestino(destino);
-                system("cls");
+        cout << "=====================================\n";
+        cout << "              AEROLINEA\n";
+        cout << "=====================================\n";
+        cout << "1. Introducir destino\n";
+        cout << "2. Calcular cantidad de vuelos\n";
+        cout << "3. Buscar ruta\n";
+        cout << "4. Listar destinos\n";
+        cout << "0. Salir\n";
+        cout << "Seleccione una opcion: ";
 
-    } else if(respuesta == "2"){
+        getline(cin, opcion);
 
-                system("cls");
-                cout<<"Ingrese el pais origen"<<endl;
-                cin>>origen;
-                cout<<"Ingrese el pais destino"<<endl;
-                cin>>destino;
-                finaI = paises.cuenta(origen,destino);
-                cout<< "Esta a " << finaI << " vuelos de llegar a su destino "<<endl <<endl;
+        limpiarPantalla();
 
-    }else if(respuesta == "3"){
+        if (opcion == "1") {
+            string destino = leerTexto("Ingrese el nuevo destino: ");
+            paises.introducirDestino(destino);
 
-                system("cls");
+            cout << "\nDestino agregado correctamente.\n";
+            pausar();
+        }
+        else if (opcion == "2") {
+            string origen = leerTexto("Ingrese el origen: ");
+            string destino = leerTexto("Ingrese el destino: ");
 
-                cout<<"Ingrese el pais origen"<<endl;
-                getline(cin,s);
+            int vuelos = paises.contarVuelos(origen, destino);
 
-                cout<<" Ingrese el pais destino"<<endl;
-                getline(cin,j);
+            if (vuelos == -1) {
+                cout << "\nNo existe una ruta valida entre esos destinos.\n";
+            } else {
+                cout << "\nEsta a " << vuelos << " vuelo(s) de llegar a su destino.\n";
+            }
 
-                cout<< paises.buscarRuta(s,j) << endl <<endl;
+            pausar();
+        }
+        else if (opcion == "3") {
+            string origen = leerTexto("Ingrese el origen: ");
+            string destino = leerTexto("Ingrese el destino: ");
 
-
-    }
-
-
+            cout << "\n" << paises.buscarRuta(origen, destino) << endl;
+            pausar();
+        }
+        else if (opcion == "4") {
+            paises.listarDestinos();
+            pausar();
+        }
+        else if (opcion == "0") {
+            cout << "\nSaliendo del sistema...\n";
+        }
+        else {
+            cout << "\nOpcion invalida.\n";
+            pausar();
         }
 
+    } while (opcion != "0");
+
+    return 0;
 }
-
-
-
